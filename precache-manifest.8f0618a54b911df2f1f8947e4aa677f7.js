4c44b1e2844e6f21191c8960880a155fa42547b7
Mo <mohammed4685@outlook.com>
2020-10-09 22:12:14 +0100

    Updates

---
+++ precache-manifest.8f0618a54b911df2f1f8947e4aa677f7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8276d6442824bd8766cce5bf6ded01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d3aeaa9c4451b31acbb",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "1d9967dd6dd77dc9bbb2",
+    "url": "/static/css/main.d3f1b077.chunk.css"
+  },
+  {
+    "revision": "5d3aeaa9c4451b31acbb",
+    "url": "/static/js/2.d0aab548.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d0aab548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9967dd6dd77dc9bbb2",
+    "url": "/static/js/main.5ecd6d24.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "f6ce3558870d594922c86278d4f1c06e",
+    "url": "/static/media/Rokkitt-Medium.f6ce3558.ttf"
+  },
+  {
+    "revision": "51fd37208a8c9cfd835f5fb071888f83",
+    "url": "/static/media/csharplogo.51fd3720.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3ea353cdc90408d803760d1d3e59db5d",
+    "url": "/static/media/htmlogo.3ea353cd.png"
+  },
+  {
+    "revision": "652fdb659a681116811612e0b9e25354",
+    "url": "/static/media/javalogo.652fdb65.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0fbd04cba8e061b55e9da307a880f8f3",
+    "url": "/static/media/mongologo.0fbd04cb.png"
+  },
+  {
+    "revision": "67db45dce58818968867e8230254528c",
+    "url": "/static/media/nodelogo.67db45dc.png"
+  },
+  {
+    "revision": "88ec78cfe4c2cb212081eb3287c185c4",
+    "url": "/static/media/phplogo.88ec78cf.png"
+  },
+  {
+    "revision": "7104884eb61bfa53e300ae7bdc330603",
+    "url": "/static/media/profilepic.7104884e.jpg"
+  },
+  {
+    "revision": "70f87bba34487dc1c0cb8916d800c63b",
+    "url": "/static/media/react-logo.png.70f87bba.png"
+  },
+  {
+    "revision": "1ee431affc5d1e75028b0076a3cc5700",
+    "url": "/static/media/sqllogo.1ee431af.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MoAziz123.github.io.git/precache-manifest.5a65f2c9fd4343f94c0e2c22339c3fbe.js"
+  "/precache-manifest.8f0618a54b911df2f1f8947e4aa677f7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MoAziz123.github.io.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
